249448b386bfff141a41c67ac4ff60eb47fdc63e
Luana Leal <luana.leal.eq@gmail.com>
2020-08-26 02:03:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize (name)
+    @@name = name 
+    @@save = save 
+   # @@all << self 
+    
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.print_all
+    self.all.each{|name| puts "#{name}"}
+  end 
+  
+  def save 
+    self.class.all << self 
+  end 
+  
+  #def name=(dogs_name)
+   #@@name = dogs_name
+  #end 
+  
+   def self.name
+     @@name 
+   end 
+end 
